79c32715a1a6d8cb118c6ac472fa08f65daf0cf4
dhythm <yuta.okada.20@gmail.com>
2022-07-03 10:47:01 +0900

    implement DataGrid [WIP]

--- src/components/DataGrid/DataGrid.stories.tsx
+++ src/components/DataGrid/DataGrid.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { DataGrid } from "./DataGrid";
-import { SampleTable } from "./SampleTable";
 
 export default {
   title: "Component/DataGrid",
@@ -9,7 +8,118 @@ export default {
 } as ComponentMeta<typeof DataGrid>;
 
 const Template: ComponentStory<typeof DataGrid> = (args) => (
-  <DataGrid {...args} />
+  <DataGrid header={header} rows={rows} />
 );
+
+const header = [
+  { name: "A" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D" },
+  { name: "E" },
+];
+
+const rows = [
+  {
+    name: "Frozen yoghurt",
+    calories: 159,
+    fat: 6,
+    carbs: 24,
+    protein: 4,
+    price: 3.99,
+    history: [
+      {
+        date: "2020-01-05",
+        customerId: "11091700",
+        amount: 3,
+      },
+      {
+        date: "2020-01-02",
+        customerId: "Anonymous",
+        amount: 1,
+      },
+    ],
+  },
+  {
+    name: "Ice cream sandwich",
+    calories: 237,
+    fat: 9,
+    carbs: 37,
+    protein: 4.3,
+    price: 4.99,
+    history: [
+      {
+        date: "2020-01-05",
+        customerId: "11091700",
+        amount: 3,
+      },
+      {
+        date: "2020-01-02",
+        customerId: "Anonymous",
+        amount: 1,
+      },
+    ],
+  },
+  {
+    name: "Eclair",
+    calories: 262,
+    fat: 16,
+    carbs: 24,
+    protein: 6,
+    price: 3.79,
+    history: [
+      {
+        date: "2020-01-05",
+        customerId: "11091700",
+        amount: 3,
+      },
+      {
+        date: "2020-01-02",
+        customerId: "Anonymous",
+        amount: 1,
+      },
+    ],
+  },
+  {
+    name: "Cupcake",
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    price: 2.5,
+    history: [
+      {
+        date: "2020-01-05",
+        customerId: "11091700",
+        amount: 3,
+      },
+      {
+        date: "2020-01-02",
+        customerId: "Anonymous",
+        amount: 1,
+      },
+    ],
+  },
+  {
+    name: "Gingerbread",
+    calories: 356,
+    fat: 16,
+    carbs: 49,
+    protein: 3.9,
+    price: 1.5,
+    history: [
+      {
+        date: "2020-01-05",
+        customerId: "11091700",
+        amount: 3,
+      },
+      {
+        date: "2020-01-02",
+        customerId: "Anonymous",
+        amount: 1,
+      },
+    ],
+  },
+];
 
 export const $DataGrid = Template.bind({});
--- src/components/DataGrid/DataGrid.tsx
+++ src/components/DataGrid/DataGrid.tsx
@@ -10,19 +10,21 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
   Typography,
 } from "@mui/material";
-import { FC, useReducer, useState } from "react";
+import { ChangeEvent, ComponentProps, FC, useReducer, useState } from "react";
 
-type Props = {};
+type Props = Pick<ComponentProps<typeof CollapsibleTable>, "header" | "rows">;
 
-export const DataGrid: FC<Props> = () => {
+export const DataGrid: FC<Props> = ({ header, rows }) => {
   const rerender = useReducer(() => ({}), {})[1];
-  return <CollapsibleTable />;
+  return <CollapsibleTable header={header} rows={rows} />;
 };
 
-const Row: FC<{ row: ReturnType<typeof createData> }> = ({ row }) => {
+type RowProps<T extends { name: string } = any> = T;
+const Row: FC<{ row: RowProps }> = ({ row }) => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -48,35 +50,7 @@ const Row: FC<{ row: ReturnType<typeof createData> }> = ({ row }) => {
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
+            <CollapsedComponent row={row} />
           </Collapse>
         </TableCell>
       </TableRow>
@@ -84,62 +58,87 @@ const Row: FC<{ row: ReturnType<typeof createData> }> = ({ row }) => {
   );
 };
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-  price: number
-) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        date: "2020-01-05",
-        customerId: "11091700",
-        amount: 3,
-      },
-      {
-        date: "2020-01-02",
-        customerId: "Anonymous",
-        amount: 1,
-      },
-    ],
-  };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-  createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-  createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
-  createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
-];
-
-const CollapsibleTable: FC<{}> = () => (
-  <TableContainer component={Paper}>
-    <Table aria-label="collapsible table">
+type CollapsedComponentProps<T extends { name: string } = any> = {
+  row: RowProps<T>;
+};
+const CollapsedComponent: FC<CollapsedComponentProps> = ({ row }) => (
+  <Box sx={{ margin: 1 }}>
+    <Typography variant="h6" gutterBottom component="div">
+      History
+    </Typography>
+    <Table size="small" aria-label="purchases">
       <TableHead>
         <TableRow>
-          <TableCell />
-          <TableCell>Dessert (100g serving)</TableCell>
-          <TableCell align="right">Calories</TableCell>
-          <TableCell align="right">Fat&nbsp;(g)</TableCell>
-          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-          <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableCell>Date</TableCell>
+          <TableCell>Customer</TableCell>
+          <TableCell align="right">Amount</TableCell>
+          <TableCell align="right">Total price ($)</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
-        {rows.map((row) => (
-          <Row key={row.name} row={row} />
+        {row.history.map((historyRow) => (
+          <TableRow key={historyRow.date}>
+            <TableCell component="th" scope="row">
+              {historyRow.date}
+            </TableCell>
+            <TableCell>{historyRow.customerId}</TableCell>
+            <TableCell align="right">{historyRow.amount}</TableCell>
+            <TableCell align="right">
+              {Math.round(historyRow.amount * row.price * 100) / 100}
+            </TableCell>
+          </TableRow>
         ))}
       </TableBody>
     </Table>
-  </TableContainer>
+  </Box>
 );
+
+type Header = { name: string }[];
+type TableProps<T extends { name: string } = any> = {
+  header: Header;
+  rows: RowProps<T>[];
+};
+
+const CollapsibleTable: FC<TableProps> = ({ header, rows }) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  return (
+    <Paper>
+      <TableContainer>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              {header.map((element) => (
+                <TableCell>{element.name}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <Row key={row.name} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+};
